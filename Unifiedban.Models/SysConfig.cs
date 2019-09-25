5f196599a29c179f2afa97b579cd9cc41d879c48
Pietro di Caprio <pietro.dicaprio@subpixel.it>
2019-09-25 07:20:08 +0200

    (WIP) Task terminal#8 - Add SysConfig entity

---
+++ Unifiedban.Models/SysConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Unifiedban.Models
+{
+    [Table("SysConfig", Schema = "config")]
+    public class SysConfig
+    {
+        [MaxLength(50)]
+        public string SysConfigId { get; set; }
+        public string Value { get; set; }
+    }
+}
