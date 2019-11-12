fd6053bfebae5557a0dfae8546752719e478a3c4
Pietro di Caprio <pietro.dicaprio@subpixel.it>
2019-11-12 07:01:41 +0100

    (WIP) Task terminal#7 - Group configuration parameters definition

---
+++ Unifiedban.Models/Group/ConfigurationParameter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Unifiedban.Models.Group
+{
+    [Table("Group_DefaultConfigParameter", Schema = "config")]
+    public class ConfigurationParameter
+    {
+        [Key]
+        public string ConfigurationParameterId { get; set; }
+        public string Value { get; set; }
+        public string Type { get; set; } // bool, string, int
+        public string Values { get; set; } // possible values ; separated
+    }
+}
