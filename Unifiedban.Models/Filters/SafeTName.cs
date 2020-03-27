71a7859efd746b7345276dd799c1b6859a9f1053
Pietro di Caprio <pietro.dicaprio@subpixel.it>
2020-03-27 23:31:05 +0100

    Add Telegram Safe Names

---
+++ Unifiedban.Models/Filters/SafeTName.cs
@@ -0,0 +1,20 @@
+﻿/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Unifiedban.Models.Filters
+{
+    [Table("Filter_SafeTName", Schema = "dbo")]
+    public class SafeTName
+    {
+        [MaxLength(100)]
+        public string Username { get; set; }
+        public long TelegramChatId { get; set; }
+        public DateTime RecordDate { get; set; } = DateTime.UtcNow;
+    }
+}
