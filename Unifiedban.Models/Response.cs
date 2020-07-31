bc0d3d2e8ef96c9f73acc81e9300692ef3c8577c
Pietro di Caprio <pietro.dicaprio@subpixel.it>
2020-08-01 00:22:59 +0200

    Add Response model

---
+++ Unifiedban.Models/Response.cs
@@ -0,0 +1,16 @@
+﻿/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Unifiedban.Models
+{
+    [NotMapped]
+    public class Response
+    {
+        public int StatusCode { get; set; }
+        public string StatusDescription { get; set; }
+        public object Payload { get; set; }
+    }
+}
